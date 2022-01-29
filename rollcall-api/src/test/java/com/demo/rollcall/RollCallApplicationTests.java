963777ea7d884e6d0058876f6c6133b3894e6974
saravana2906 <saravana2906@gmail.com>
2022-01-29 17:27:48 +0000

    intial-commit

---
+++ rollcall-api/src/test/java/com/demo/rollcall/RollCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.rollcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
